b6bb36e5bbeb3201e0972ad87ba03b8a7afa5d8b
lesterietado <leandrocabrera7c@gmail.com>
2025-09-25 17:02:23 -0300

    Creación de vistas / componentes - Uso de rutas dinamicas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Contacts } from './pages/contacts/contacts';
+import { About } from './pages/about/about';
+import { Products } from './pages/products/products';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: Home},
+    {path: 'contacts', component: Contacts},
+    {path: 'about', component: About},
+    {path: 'products', component: Products}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { About } from './pages/about/about';
+import { Home } from './pages/home/home';
+import { Contacts } from './pages/contacts/contacts';
+import { Products } from './pages/products/products';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, About, Home, Contacts, Products],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
