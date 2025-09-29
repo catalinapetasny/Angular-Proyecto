191629e77a487bbbe4f168979e41658f00fbf0de
lvscataa <petasnycatalina@gmail.com>
2025-09-29 17:11:58 -0300

    Cambios en la pagina

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { About } from './pages/about/about';
-import { Home } from './pages/home/home';
-import { Contacts } from './pages/contacts/contacts';
-import { Products } from './pages/products/products';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, About, Home, Contacts, Products],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
